3d10ce88e91200156bea5efa53c874481a0661fe
Josue Ouedraogo <pawendtaoreo92@gmail.com>
2026-06-17 11:46:20 +0100

    changement de protocol

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,23 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (app()->environment('local')) {
-
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
-
         }
     }
 }
